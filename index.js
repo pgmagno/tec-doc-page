a6e4bdcaa0ec872880c90b45b4c05f26860335d2
Paulo Giovanny <pgs.magno@gmail.com>
2021-08-25 01:39:29 -0300

    fix collapsable bar, added footers and responsiveness

---
+++ index.js
@@ -0,0 +1,9 @@
+var elem = document.getElementById("items-top");
+
+var elem3 = document.getElementById("arrow");
+
+function slide() {
+  elem.classList.toggle('hide');
+  elem3.classList.toggle('rotate');
+
+}
